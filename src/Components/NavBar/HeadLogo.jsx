a283ca89ecdd689da14611ff11913c23da72078e
sadiq <sadiq.creatik@gmail.com>
2024-09-19 17:36:41 +0300

    Nav Bar Finished

--- src/Components/NavBar/HeadLogo.jsx
+++ src/Components/NavBar/HeadLogo.jsx
@@ -6,20 +6,20 @@ function HeadLogo() {
     <>
       <div className="container mt-1">
         <div className="row">
-          <div className="col-2 log">
-            <span>
-              <strong>E-Commerse<br/>Application</strong>
+          <div className="col-md-2 log">
+            <span className="justify-content-center align-middle">
+              <strong>E-Commerse Application</strong>
             </span>
           </div>
-          <div className="col-7">
+          <div className="col-md-7">
             <div class="input-group">
               <input type="text" className="form-control" />
               <span className="input-group-text"><i class="fa fa-search" aria-hidden="true"></i></span>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-md-3">
             <div className="row">
-                <div className="col-5 recent-list">
+                <div className="col-xs-4 col-sm-4 col-md-4 col-6 recent-list">
                   
                 <i class="fa fa-list-alt" aria-hidden="true"></i>
                 <span><a href="">Recently Viewed</a></span>
